3b04e3840722abac6ea8bd2ff7fcc4c68778c896
Lucas Gaspar <lucasprgaspar@gmail.com>
2020-02-12 20:13:54 -0300

    Update Jogadores.cpp

--- Jogadores.cpp
+++ Jogadores.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string.h>
 #include <iomanip>
-#include <string>
 #include <vector>
 
 using namespace std;
@@ -11,12 +10,11 @@ int main(){
     int i;
     int N;
     int idade[1000];
-    string nome[1000];
+    char nome[1000];
 
     cin >> N;
-
+    cin.getline(nome,1000);
     for(i=0;i<N;i++){
-         getline(cin, nome[i]);
          cin >> idade[i];
          cout << nome[i] << idade [i] << endl;
     }
