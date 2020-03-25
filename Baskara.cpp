75e5a1eb72e421a025f517d0ebea48d1e17ca98c
Julia Daniele <juliadanielesilva@outlook.com>
2020-03-25 16:01:00 -0300

    Correção de erros

--- Baskara.cpp
+++ Baskara.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
- 
+#include <cmath>
+
 using namespace std;
- 
-int main() {
- 
-   	int N;
-	int i, i1, i2;
-	int V[1000];
-	
-	cin >> N;
-	
-	V[0]=0;
-	V[1]=1;
-	
-	for(i=2;i<N;i++){
-		
-		i1 = i-1;
-		i2 = i-2;
-		V[i] = V[i1] + V[i2];
-		
-    }
-    
-    for(i=0;i<(N-1);i++)
-    	cout << V[i] << " ";
-    
-    cout << V[i] << endl;
- 
-    return 0;
+
+int main ()
+{
+	double A, B, C;
+	double d, delta;
+	double R1, R2;
+
+	cin >> A >> B >> C;
+
+	d = (B * B) - (4 * A * C);
+
+	if (d<0 || A == 0)
+		cout << "Impossivel calcular" << endl;
+	else
+	{
+		delta = sqrt(d);
+		R1 = (-B + delta) / (2 * A);
+		R2 = (-B - delta) / (2 * A);
+
+		cout << fixed;
+		cout.precision(5);
+		cout << "R1 = " << R1 << endl;
+		cout << "R2 = " << R2 << endl;
+	}
+
+	return 0;
 }
--- Primo.cpp
+++ Primo.cpp
@@ -5,44 +5,41 @@ using namespace std;
 
 int main()
 {
-	int vezes;
-	cin >> vezes;
 
-	for (int t = 0; t <  vezes; t++)
+	int n; //numero lido
+	int r; //raiz quadrada
+	int i = 0; //contador
+	bool f = true; //flag para indentificar primo
+
+	// Ler o numero
+	cin >> n;
+
+	/* Se um numero nÃ£o for divisivel por nenhum numero entre 1 e a raiz quadrada dele,
+	* com excessao do 1, ele e primo.
+	* Com essa abordagem, se torna possÃ­vel reduzir o tempo de execuÃ§Ã£o
+	*/
+	
+	r = sqrt (n);
+
+	if(n == 1 || n == 2) //Um e dois sao primos
 	{
-		int n; //numero lido
-		int r; //raiz quadrada
-		int i = 0; //contador
-		bool f = true; //flag para indentificar primo
-
-		// Ler o numero
-		cin >> n;
-
-		/* Se um numero não for divisivel por nenhum numero entre 1 e a raiz quadrada dele,
-		* com excessao do 1, ele e primo.
-		* Com essa abordagem, se torna possível reduzir o tempo de execução
-		*/
-
-		r = sqrt (n);
-
-		if(n == 1 || n == 2) //Um e dois sao primos
-		{
-			cout << n << " eh primo" << endl;
-		}
-		else
-		{
-			for(i = 2; i <= r; i++) //teste até a raiz quadrada
-			{
-				if (n % i == 0)
-				{
-					f = false;
-					break;
-				}
-			}
-
-			if (f) cout << n << " eh primo" << endl;
-			else cout << n << " nao eh primo" << endl;
-		}
+		cout << "primo" << endl;
 	}
+	else
+	{
+		for(i = 2; i <= r; i++) //teste atÃ© a raiz quadrada
+		{
+			if (n % i == 0)
+			{
+				f = false;
+				break;
+			}
+		}
+
+		if (f) cout << "primo" << endl;
+		else cout << "nao e primo" << endl;
+	}
+
 	return 0;
 }
+
